61716f0bb4eb85e5fdb5d7ba4c6941590ed4747f
size314 <37399132+size314@users.noreply.github.com>
2024-04-17 10:29:32 +0200

    Add Maven project
    Added Maven project from https://start.spring.io/

---
+++ src/main/java/com/size314/proyectoLambda/ProyectoLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.size314.proyectoLambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/size314/proyectoLambda/ProyectoLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.size314.proyectoLambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
